e297278c2ff737c3a8f15db239d77c7ea0141f22
Olwenyu <van.huynhcsolwenyu@hcmut.edu.vn>
2026-04-26 11:25:38 +0700

    simple layout for order

---
+++ frontend/src/main.jsx
@@ -0,0 +1,21 @@
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { OrderProvider } from "./context/OrderContext";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <OrderProvider>
+    <App />
+  </OrderProvider>
+);
